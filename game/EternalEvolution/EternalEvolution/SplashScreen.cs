1d38ac86725125944fa8a381045d3a0b69653048
ミキ Wawrzyniak <mikolajspejs@gmail.com>
2016-10-21 22:14:03 +0200

    fixed xml serialization/deserialization

--- game/EternalEvolution/EternalEvolution/SplashScreen.cs
+++ game/EternalEvolution/EternalEvolution/SplashScreen.cs
@@ -6,13 +6,20 @@ using System.Threading.Tasks;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
+using System.Xml.Serialization;
 
 namespace EternalEvolution
 {
+    public class Image
+    {
+        public string Path;
+    }
+
     public class SplashScreen : GameScreen
     {
         Texture2D image;
-        public string Path;
+        public Image Image;
+        public Vector2 Position;
 
         public SplashScreen()
         {
@@ -23,7 +30,7 @@ namespace EternalEvolution
         {
             base.LoadContent();
             this.Content = new ContentManager(ScreenManager.Instance.Content.ServiceProvider, "Content");
-            image = Content.Load<Texture2D>(Path);
+            image = Content.Load<Texture2D>(path[0]);
         }
 
         public override void UnloadContent()
@@ -38,7 +45,7 @@ namespace EternalEvolution
 
         public override void Draw(SpriteBatch spriteBatch)
         {
-            spriteBatch.Draw(image, Vector2.Zero, Color.White);
+            spriteBatch.Draw(image, Position, Color.White);
             //base.Draw(spriteBatch);
         }
     }
