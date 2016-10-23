5f20a545f2a5235eec11554a362ecbc7b650d703
ミキ Wawrzyniak <mikolajspejs@gmail.com>
2016-10-23 16:45:32 +0200

    turned debug mode off

--- game/EternalEvolution/EternalEvolution/ScreenManager.cs
+++ game/EternalEvolution/EternalEvolution/ScreenManager.cs
@@ -33,13 +33,13 @@ namespace EternalEvolution
         public ScreenManager()
         {
             Dimensions = new Vector2(640, 480);
-            //currentScreen = new SplashScreen();
-            currentScreen = new GameplayScreen();
+            currentScreen = new SplashScreen();
+            //currentScreen = new GameplayScreen();
             xmlGameScreenManager = new XmlManager<GameScreen>();
             xmlGameScreenManager.Type = currentScreen.Type;
-            //currentScreen = xmlGameScreenManager.Load("Load/SplashScreen.xml");
-            
-         }
+            currentScreen = xmlGameScreenManager.Load("Load/SplashScreen.xml");
+
+        }
         public static ScreenManager Instance
         {
             get
